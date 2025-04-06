95995e8e917b84d278e761425ca6d2c81f0092c7
TechXNinjas <thetechxninjas@gmail.com>
2025-04-06 21:34:49 +0530

    Update TechXNinjas.sh

--- TechXNinjas.sh
+++ TechXNinjas.sh
@@ -309,51 +309,6 @@ EOM
 
 gcloud beta container binauthz policy import binauth_policy.yaml
 
-CONTAINER_PATH=$REGION-docker.pkg.dev/${PROJECT_ID}/artifact-scanning-repo/sample-image
-
-DIGEST=$(gcloud container images describe ${CONTAINER_PATH}:good \
-    --format='get(image_summary.digest)')
-
-cat > deploy.yaml << EOM
-apiVersion: v1
-kind: Service
-metadata:
-  name: deb-httpd
-spec:
-  selector:
-    app: deb-httpd
-  ports:
-    - protocol: TCP
-      port: 80
-      targetPort: 8080
----
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: deb-httpd
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: deb-httpd
-  template:
-    metadata:
-      labels:
-        app: deb-httpd
-    spec:
-      containers:
-      - name: deb-httpd
-        image: ${CONTAINER_PATH}@${DIGEST}
-        ports:
-        - containerPort: 8080
-        env:
-          - name: PORT
-            value: "8080"
-
-EOM
-
-kubectl apply -f deploy.yaml
-
 docker build -t $REGION-docker.pkg.dev/${PROJECT_ID}/artifact-scanning-repo/sample-image:bad .
 
 docker push $REGION-docker.pkg.dev/${PROJECT_ID}/artifact-scanning-repo/sample-image:bad
